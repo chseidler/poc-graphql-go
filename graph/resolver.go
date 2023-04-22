8293d5aaa51d380421bb21e721347a9f1e06dbb3
Christian Seidler <ch.cseidler@gmail.com>
2023-04-21 21:33:45 -0300

    Category resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,13 @@
 package graph
 
+import (
+	"github.com/chseidler/poc-graphql-go/internal/database"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	CategoryDB *database.Category
+}
